ae0fd57b9c06cb3b38949826316eb34781431e08
“Suraj <bastolasuraj@outlook.com>
2020-06-13 14:12:51 +0545

    Third Commit

--- wp-content/themes/sb-portfolio/templates/homepage.php
+++ wp-content/themes/sb-portfolio/templates/homepage.php
@@ -89,6 +89,147 @@ endif;
 ?>
     <!--About Section-->
 
+    <!--Services Section-->
+<?php
+$args  = array(
+	'page_id' => 62
+);
+$query = new WP_Query( $args );
+if ( $query->have_posts() ):
+	while ( $query->have_posts() ):
+		$query->the_post();
+		$attachment = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'large', true );
+		?>
+        <div id="colorlib-services">
+            <div class="container">
+                <div class="row text-center">
+                    <h2 class="bold"><?php the_title(); ?></h2>
+                </div>
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="services-flex">
+                            <div class="one-third">
+                                <div class="row">
+                                    <div class="col-md-12 col-md-offset-0 animate-box intro-heading">
+                                        <span><?php echo get_field( 'services_helper_title' ); ?></span>
+                                        <h2><?php echo get_field( 'services_title' ); ?></h2>
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <div class="col-md-12">
+                                        <div class="rotate">
+                                            <h2 class="heading"><?php the_title(); ?></h2>
+                                        </div>
+                                    </div>
+									<?php
+									if ( have_rows( 'services' ) ):
+										$sn = 1;
+										while ( have_rows( 'services' ) ):
+											the_row();
+											?>
+                                            <div class="col-md-6">
+                                                <div class="services animate-box">
+                                                    <h3><?php echo $sn . ' - ' . get_sub_field( 'service_provided' ); ?></h3>
+                                                    <ul>
+														<?php
+														if ( have_rows( 'service_options_repeater' ) ):
+															while ( have_rows( 'service_options_repeater' ) ):
+																the_row();
+																?>
+                                                                <li> <?php echo get_sub_field( 'service_options' ); ?> </li>
+															<?
+															endwhile;
+														endif;
+														?>
+                                                    </ul>
+                                                </div>
+                                            </div>
+											<?php
+											$sn ++;
+											if ( $sn > 4 ) {
+												break;
+											}
+										endwhile;
+									endif;
+									?>
+                                </div>
+                            </div>
+                            <div class="one-forth services-img"
+                                 style="background-image: url(<?php echo $attachment[0]; ?>);">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+	<?php
+	endwhile;
+	wp_reset_postdata();
+endif;
+?>
+    <!--Services Section-->
 
+    <!--Portfolio Section-->
+    <div id="colorlib-work">
+        <div class="container">
+            <div class="row text-center">
+				<?php
+				$post_type = get_post_type( get_the_ID() );
+				?>
+                <h2 class="bold"><?php echo $post_type; ?></h2>
+            </div>
+            <div class="row">
+                <div class="col-md-12 col-md-offset-0 text-center animate-box intro-heading">
+                    <span>Portfolio</span>
+                    <h2>Done Projects</h2>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="rotate">
+                        <h2 class="heading">Portfolio</h2>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+				<?php
+				$args  = array(
+					'post_type'      => 'works',
+					'posts_per_page' => 4,
+				);
+				$query = new WP_Query( $args );
+				if ( $query->have_posts() ):
+					while ( $query->have_posts() ):
+						$query->the_post();
+				$attachment = wp_get_attachment_image_src(get_post_thumbnail_id($post->ID),'large',true);
+						?>
+                        <div class="col-md-12">
+                            <div class="work-entry animate-box">
+                                <a href="<?php the_permalink(); ?>" class="work-img" style="background-image: url(<?php echo $attachment[0]; ?>);">
+                                    <div class="display-t">
+                                        <div class="work-name">
+                                            <h2><?php the_title()?></h2>
+                                        </div>
+                                    </div>
+                                </a>
+                                <div class="col-md-4 col-md-offset-4">
+                                    <div class="desc">
+                                        <p><?php the_content(); ?></p>
+                                        <p class="read"><a href="<?php echo get_field('live_site'); ?>">View details</a></p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+					<?php
+
+					endwhile;
+					wp_reset_postdata();
+				endif;
+				?>
+            </div>
+        </div>
+    </div>
+
+    <!--Portfolio Section-->
 <?php
 get_footer();
